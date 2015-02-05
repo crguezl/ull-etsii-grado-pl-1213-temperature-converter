46dba135bc4c0deabd22d96bb345479003da52ed
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2015-02-05 21:35:30 +0000

    task minify in gulpfile improved

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,23 @@ var gulp    = require('gulp'),
     gutil   = require('gulp-util'),
     uglify  = require('gulp-uglify'),
     concat  = require('gulp-concat');
-var karma = require('gulp-karma');
-var del = require('del');
+var karma   = require('gulp-karma');
+var del     = require('del');
+var minifyHTML = require('gulp-minify-html');
+var minifyCSS  = require('gulp-minify-css');
 
 gulp.task('minify', function () {
   gulp.src('temperature.js')
   .pipe(uglify())
   .pipe(gulp.dest('minified'));
+
+  gulp.src('./index.html')
+    .pipe(minifyHTML())
+    .pipe(gulp.dest('./minified/'))
+
+  gulp.src('./*.css')
+   .pipe(minifyCSS({keepBreaks:true}))
+   .pipe(gulp.dest('./minified/'))
 });
 
 
