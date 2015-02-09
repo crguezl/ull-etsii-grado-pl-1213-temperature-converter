897bea6d05a771b4713e4f33bffe93936c9ba6c6
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2015-02-09 14:37:38 +0000

    added script karma to package.json

--- gulpfile.js
+++ gulpfile.js
@@ -22,16 +22,9 @@ gulp.task('minify', function () {
 });
 
 
-var testFiles = [
-  'client/todo.js',
-  'client/todo.util.js',
-  'client/todo.App.js',
-  'test/client/*.js'
-];
-
 gulp.task('test', function() {
   // Be sure to return the stream
-  return gulp.src(testFiles)
+  return gulp.src([])
     .pipe(karma({
       configFile: 'karma.conf.js',
       action: 'run'
@@ -43,7 +36,7 @@ gulp.task('test', function() {
 });
 
 gulp.task('default', function() {
-  gulp.src(testFiles)
+  gulp.src([])
     .pipe(karma({
       configFile: 'karma.conf.js',
       action: 'watch'
